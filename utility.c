28a0aaf30c814c91291c7fac3288b802e6ad5d17
OakEW <edward.y.wang@outlook.com>
2026-01-14 22:59:28 +0100

    Fix memory leak in clean_up function
    Added a check to free color_index in clean_up function.

--- utility.c
+++ utility.c
@@ -104,7 +104,9 @@ int	ft_strcmp(const char *s1, const char *s2)
 void	clean_up(t_data *fractol)
 {
 	if (fractol->iters)
-    	free(fractol->iters);
+		free(fractol->iters);
+	if (fractol->color_index)
+		free(fractol->color_index);
 	if (fractol->img)
 		mlx_destroy_image(fractol->mlx, fractol->img);
 	if (fractol->win)
