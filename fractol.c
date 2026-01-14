57894afb1b516225132a0010575f533514b41ec0
OakEW <edward.y.wang@outlook.com>
2026-01-14 23:49:28 +0100

    Update fractol.c

--- fractol.c
+++ fractol.c
@@ -12,15 +12,29 @@
 
 #include "fractol.h"
 
-t_data	init_help(t_data *fractol)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ywang2 <ywang2@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 14:53:54 by ywang2            #+#    #+#             */
+/*   Updated: 2026/01/14 23:48:05 by ywang2           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	init_help(t_data *fractol)
 {
+	fractol->w = 1200;
+	fractol->h = 600;
 	fractol->ix = 0.0;
 	fractol->iy = 0.0;
 	fractol->re = 0.0;
 	fractol->im = 0.0;
 	fractol->zoom = 1.0;
-	fractol->w = 1200;
-	fractol->h = 600;
 	fractol->set = 2;
 	fractol->off_x = 0.0;
 	fractol->off_y = 0.0;
@@ -28,13 +42,15 @@ t_data	init_help(t_data *fractol)
 	fractol->shift = 1.3;
 	fractol->animate = 1;
 	fractol->palette_set = 1;
-	fractol->color_index = malloc(sizeof(int) * fractol.w * fractol.h);
+	fractol->color_index = malloc(sizeof(int) * fractol->w * fractol->h);
 	if (!fractol->color_index)
-	{
-    	clean_up(&fractol);
-    	exit(5);
-	}
-	return (*fractol);
+		handle_errors(fractol);
+}
+
+void	handle_errors(t_data *fractol)
+{
+	clean_up(fractol);
+	exit (1);
 }
 
 t_data	init_all(char **argv)
@@ -44,26 +60,17 @@ t_data	init_all(char **argv)
 	init_help(&fractol);
 	fractol.mlx = mlx_init();
 	if (!fractol.mlx)
-		exit (1);
+		handle_errors(&fractol);
 	fractol.win = mlx_new_window(fractol.mlx, fractol.w, fractol.h, argv[1]);
 	if (!fractol.win)
-	{
-		clean_up(&fractol);
-		exit (2);
-	}
+		handle_errors(&fractol);
 	fractol.img = mlx_new_image(fractol.mlx, fractol.w, fractol.h);
 	if (!fractol.img)
-	{
-		clean_up(&fractol);
-		exit (3);
-	}
+		handle_errors(&fractol);
 	fractol.addr = mlx_get_data_addr(fractol.img, &fractol.bits_per_pixel,
 			&fractol.line_length, &fractol.endian);
 	if (!fractol.addr)
-	{
-		clean_up(&fractol);
-		exit(4);
-	}
+		handle_errors(&fractol);
 	rainbow_palette(&fractol);
 	return (fractol);
 }
